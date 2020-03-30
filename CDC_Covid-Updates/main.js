9ef283ec91e9f595d8873a5f311dad546ea20058
essjay05 <stepjoyous@gmail.com>
2020-03-30 01:38:20 -0700

    Successfully returned CDC Coronavirus query as json.

--- CDC_Covid-Updates/main.js
+++ CDC_Covid-Updates/main.js
@@ -1,6 +1,19 @@
-const CDC_RSS_URL = `https://tools.cdc.gov/api/v2/resources/media/403372.rss`;
+// URL provided by Tim for challenge
+const CDC_FEED_URL = `https://tools.cdc.gov/medialibrary/index.aspx#/feed/id/403372`;
+// URL found by reading CDC api documentation
+const CDC_COVID_API = `https://tools.cdc.gov/api/v2/resources/media?topic=coronavirus`;
 
-fetch(CDC_RSS_URL)
-    .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, `text/xml`))
-    .then(data => console.log(data))
+async function getCDCFeed() {
+    const response = await fetch(CDC_COVID_API);
+    const data = await response.json();
+    console.log(data);
+};
+
+getCDCFeed();
+
+// fetch(CDC_RSS_URL)
+//     .then(response => response.text())
+//     .then(str => new window.DOMParser().parseFromString(str, `text/xml`))
+//     .then(data => console.log(data))
+
+
