ee100c69bedff2a9162585697b81e4b9468466b7
essjay05 <stepjoyous@gmail.com>
2020-03-30 01:46:34 -0700

    Attempting to use the url provided by Tim to get data back.

--- CDC_Covid-Updates/main.js
+++ CDC_Covid-Updates/main.js
@@ -3,17 +3,20 @@ const CDC_FEED_URL = `https://tools.cdc.gov/medialibrary/index.aspx#/feed/id/403
 // URL found by reading CDC api documentation
 const CDC_COVID_API = `https://tools.cdc.gov/api/v2/resources/media?topic=coronavirus`;
 
+// Async function to tet CDC Covid API back as json (WORKS)
+// async function getCovidData() {
+//     const response = await fetch(CDC_COVID_API);
+//     const data = await response.json();
+//     console.log(data);
+// };
+// getCovidData();
+
+// Fetch function to retrieve CDC Covid Feed
 async function getCDCFeed() {
-    const response = await fetch(CDC_COVID_API);
+    const response = await fetch(CDC_FEED_URL);
     const data = await response.json();
     console.log(data);
 };
-
 getCDCFeed();
-
-// fetch(CDC_RSS_URL)
-//     .then(response => response.text())
-//     .then(str => new window.DOMParser().parseFromString(str, `text/xml`))
-//     .then(data => console.log(data))
 
 
